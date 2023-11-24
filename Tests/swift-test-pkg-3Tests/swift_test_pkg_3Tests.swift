fe6f938ad8a052b5ada8e766788a54a34eac7a25
Sven A. Schmidt <sas@finestructure.co>
2023-11-24 15:28:17 +0100

    Initial commit

---
+++ Tests/swift-test-pkg-3Tests/swift_test_pkg_3Tests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import swift_test_pkg_3
+
+final class swift_test_pkg_3Tests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
